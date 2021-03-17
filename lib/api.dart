cb6bbacd567ff3ee458c65700346e821c7fef980
arielmota <arieloliveira08@gmail.com>
2021-03-17 12:30:15 -0300

    full

---
+++ lib/api.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter_auth/model/cliente.dart';
+import 'package:http/http.dart' as http;
+
+
+class Api {
+  static const URL = "http://192.168.1.201:8080";
+
+
+  Future<void> cadastrarCliente(Cliente cliente) async {
+
+  }
+}
